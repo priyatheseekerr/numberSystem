bed03f6e5a92959f6140008afd92bb3af46851c4
theseekerrr <priyaseekerrr@gmail.com>
2025-04-02 15:23:12 +0530

    Convert Decimal to Octal

--- decimalToOctal.java
+++ decimalToOctal.java
@@ -1,20 +1,22 @@
 package numberSystem;
 
+//Convert Decimal to Octal
 import java.util.Scanner;
 
 public class decimalToOctal {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
         System.out.println("enter a num: ");
-        int num=sc.nextInt();
+        int num = sc.nextInt();
 
-        StringBuilder octal=new StringBuilder();
-        while(num>0){
-            int rem=num%8;
+        StringBuilder octal = new StringBuilder();
+        while (num > 0) {
+            int rem = num % 8;
             octal.append(rem);
-            num/=8;
+            num /= 8;
         }
         System.out.println(octal.reverse());
+        sc.close();
     }
 }
